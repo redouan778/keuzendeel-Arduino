2f8cc2daf0d1bbe189086229ce017e6221789684
redouan <redouan778@gmail.com>
2020-01-07 15:00:12 +0100

    opdracht 3 done

--- Arduino/libraries/sketch_jan07a/sketch_jan07a.ino
+++ Arduino/libraries/sketch_jan07a/sketch_jan07a.ino
@@ -1,12 +1,9 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  Serial.println("het werkt!"); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
 
-Installed Redouan Sellami
- m,,
+//Installed by: Redouan Sellami
